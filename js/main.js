02b63259df584e1e01dcb1237456ba6548bc9512
Otabek_Jurayev <otabek.jurayev.2002@gmail.com>
2021-12-21 04:15:31 +0500

    tab page

--- js/main.js
+++ js/main.js
@@ -2,9 +2,11 @@ $(window).scroll(function(){
     if ( $(this).scrollTop() > 50) {
         $('.header').addClass('fixed');
         $('.catalog_menu').addClass('fixed');
+        $('.header_mobile_menu').addClass('fixed');
     } else {
         $('.header').removeClass('fixed');
         $('.catalog_menu').removeClass('fixed');
+        $('.header_mobile_menu').removeClass('fixed');
     }
 });
 document.querySelector('.header__catalog').addEventListener('click', function () {
@@ -24,5 +26,23 @@ for(let i = 0; i < catalogLink.length; i++) {
         for(let k = 0; k < catalogLink.length; k++) {
             catalogLink[k].classList.remove('opacity');
         };
+    });
+};
+document.querySelector('.header_burgir').addEventListener('click', function() {
+    this.classList.toggle('active');
+    document.querySelector('.header_mobile_menu').classList.toggle('active');
+});
+
+if(document.querySelector('.question_btn')) {
+    document.querySelector('.question_btn').addEventListener('click', function() {
+        document.querySelector('.question_fixed').classList.toggle('active');
+    });
+};
+if(document.querySelector('.filter_open')) {
+    document.querySelector('.filter_open').addEventListener('click', function() {
+        document.querySelector('.categories__page__filter').classList.add('active');
+    });
+    document.querySelector('.filter_close').addEventListener('click', function() {
+        document.querySelector('.categories__page__filter').classList.remove('active');
     });
 };
